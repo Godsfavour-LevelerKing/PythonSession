063bac79ed8cefc6c1f6372c780bc6f904f40377
Godsfavour-LevelerKing <roetechhub@gmail.com>
2025-08-06 21:47:31 +0100

    Inintial commit: First Commit

--- list.py
+++ list.py
@@ -1,3 +1,67 @@
+# This is a list of 3 names
+
+students = ["Favour", "Temitope", "Fatiha"]
+
+print(students[1]) 
+
+
+print(students)
+
+ 
+#
+        #ASSIGNMENT
+#Create a list of 3 best friends 'name'
+#Print the second name
+#Print the whole list
+
+
+#Adding, removing, and Changing Items
+
+students.append("Anab")
+print(students)
+
+#Remove
+
+students.append("Table")
+students.append("chair")
+
+print(students)
+
+students.remove("Table")
+
+print(students)
+
+students.pop()
+
+print(students)
+
+#change item
+students[0] = "Abosede"
+
+print(students)
+
+
+# Assignment
+# Create a foodlist
+# add a new food
+# change a food
+# Print all the food
+
+
+# Loop with list
+
+for student in students:
+    print("Hello", student)
+
+# Assignment
+# Create a list of 5 colours. Use a loop to print:
+# "I like the colour" for each colour in the list "
+
+
+students.sort()
+
+print(students)
+
 
 
 pupils = []
